7d8c870435d1ebfba5d99619dbd1049ae55c57ff
bill <wxc9312@gmail.com>
2016-12-03 15:14:31 +0800

    add test job records

--- src/com/car/training/service/SimpleService.java
+++ src/com/car/training/service/SimpleService.java
@@ -2,6 +2,8 @@ package com.car.training.service;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * Created by bill on 11/30/16.
  */
@@ -10,4 +12,7 @@ public interface SimpleService {
 
     @Transactional
     void save(Object bean);
+
+    @Transactional
+    List getAll(Class clazz);
 }
--- src/com/car/training/service/impl/SimpleServiceImpl.java
+++ src/com/car/training/service/impl/SimpleServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * Created by bill on 11/30/16.
  */
@@ -19,5 +21,11 @@ public class SimpleServiceImpl implements SimpleService {
     @Transactional
     public void save(Object bean) {
         basedao.save(bean);
+    }
+
+    @Override
+    @Transactional
+    public List getAll(Class clazz){
+        return basedao.getAllList(clazz);
     }
 }
